a099ab520d90804aa384cb7e1a695fe2c87f5742
mattew8 <mattew4483@likelion.org>
2023-02-13 23:00:40 +0900

    BDD 기반 test code 작성
    describe, context, it 사용

--- src/List.js
+++ src/List.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 const List = ({ tasks = [] }) => {
+  if (tasks.length === 0) {
+    return <h2>할 일이 없어요</h2>;
+  }
+
   return (
     <>
       {tasks.map((task) => (
--- src/List.test.js
+++ src/List.test.js
@@ -3,22 +3,40 @@ import React from "react";
 import { render } from "@testing-library/react";
 import List from "./List";
 
+// behaivor driven development
+// 특정 행위 를 중심으로 테스트를 진행 -> describe, context, it
+
+// with tasks => renders taks
+// without tasks => renders no task message
+
 describe("List", () => {
-  it("renders tasks", () => {
-    // 각 테스트는 독립적!
-    const tasks = [
-      {
-        id: 0,
-        title: "첫 테스트",
-      },
-      {
-        id: 1,
-        title: "두번째 할 일",
-      },
-    ];
+  context("with tasks", () => {
+    it("renders tasks", () => {
+      const tasks = [
+        {
+          id: 0,
+          title: "첫 테스트",
+        },
+        {
+          id: 1,
+          title: "두번째 할 일",
+        },
+      ];
 
-    const { container } = render(<List tasks={tasks} />);
+      const { container } = render(<List tasks={tasks} />);
 
-    expect(container).toHaveTextContent("첫 테스트");
+      expect(container).toHaveTextContent("첫 테스트");
+      expect(container).toHaveTextContent("두번째 할 일");
+    });
+  });
+
+  context("without tasks", () => {
+    it("renders no tasks message", () => {
+      const tasks = [];
+
+      const { container } = render(<List tasks={tasks} />);
+
+      expect(container).toHaveTextContent("할 일이 없어요");
+    });
   });
 });
